33f027407531e20452fa42fe97f6baa10f1de220
Muhammad Sameer <136251965+MuhammadSameer18@users.noreply.github.com>
2023-06-11 22:49:53 +0500

    Add files via upload

---
+++ if condition email password check.dart
@@ -0,0 +1,24 @@
+void main() {
+  String email = "muhammadsameer123@gmail.Com";
+  String password = "12345";
+
+  String inemail = "muhammadsameer123@gmail.com";
+  String inpassword = "12345";
+
+  if (email == inemail || password == inpassword) {
+    print("right");
+  } else {
+    print("wrong");
+  }
+}
+
+  /* int num1 = 20;
+  int num2 = 20;
+  if (num1 < num2) {
+    print("num1 is less than num2");
+  } else if (num1 == num2) {
+    print("num1 equal num2");
+  } else {
+    print("num2 is less than num1");
+  }*/
+
